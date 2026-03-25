1663c12c539a582a8322a6ad20e2bfbfe57050cb
Ethan West <sqqz0154@leeds.ac.uk>
2026-03-25 15:52:48 +0000

    added comments onto port task

--- worksheet/portfolio/shapes.c
+++ worksheet/portfolio/shapes.c
@@ -35,7 +35,7 @@ Triangle makeTriangle( Point p1, Point p2, Point p3 ) {
     return new;
 }
 
-float lineLength( Line l ){
+float lineLength( Line l ){ // calculate the length of a line 
     float length, dx, dy;
     dx = l.p[1].x - l.p[0].x;
     dy = l.p[1].y - l.p[0].y;
@@ -43,19 +43,19 @@ float lineLength( Line l ){
     return length;
 } 
 
-float triangleArea( Triangle t ) {
+float triangleArea( Triangle t ) { // calculate the area of a triangle
     float area, a, b, c, s;
-    a = lineLength(makeLine(t.p[0], t.p[1]));
+    a = lineLength(makeLine(t.p[0], t.p[1])); //using the linelength function and the makeline function to calculate the length of each 
     b = lineLength(makeLine(t.p[1], t.p[2]));
     c = lineLength(makeLine(t.p[2], t.p[0]));
-    s = (a + b + c) / 2;
+    s = (a + b + c) / 2; //using heron's formula to calculate the area of the triangle
     area = sqrt(s * (s - a) * (s - b) * (s - c));
     return area;
 }
 
-bool samePoint( Point p1, Point p2 ) {
+bool samePoint( Point p1, Point p2 ) { // return true only if the point is the same location
     bool same;
-    if (fabs(p1.x - p2.x) < 1.0e-6 && fabs(p1.y - p2.y) < 1.0e-6) {
+    if (fabs(p1.x - p2.x) < 1.0e-6 && fabs(p1.y - p2.y) < 1.0e-6) { //fabs instead of abs for float values
         same = true;
     } else {
         same = false;
@@ -63,7 +63,7 @@ bool samePoint( Point p1, Point p2 ) {
     return same;
 }
 
-bool pointInLine( Point p, Line l) {
+bool pointInLine( Point p, Line l) { // return true only if the point is one of the line endpoints - one point or the other
     bool inLine;
     if (samePoint(p, l.p[0]) || samePoint(p, l.p[1])) {
         inLine = true;
@@ -73,7 +73,7 @@ bool pointInLine( Point p, Line l) {
     return inLine;
 }
 
-bool pointInTriangle( Point p, Triangle t ) {
+bool pointInTriangle( Point p, Triangle t ) { // return true only if the point is one of the triangle vertices
     bool inTriangle;
     if (samePoint(p, t.p[0]) || samePoint(p, t.p[1]) || samePoint(p, t.p[2])) {
         inTriangle = true;
