eccde31431810a8d233ebbd5ba32e00727cee7cf
Ethan West <sqqz0154@leeds.ac.uk>
2026-03-25 15:31:55 +0000

    think done port task w/o comments

--- worksheet/portfolio/main.c
+++ worksheet/portfolio/main.c
@@ -1,14 +1,28 @@
 
 #include <stdio.h>
 #include <stdbool.h>
+#include <math.h>
+#include <stdlib.h>
 
 #include "shapes.h"
 
 int main( void ) {
 
     Point p1 = makePoint(0.0,0.0);
+    Point p2 = makePoint(1.0,1.0);
+    Point p3 = makePoint(1.0,0.0);
+    Line l1 = makeLine(p1,p2);
+    Triangle t1 = makeTriangle(p1,p2,p3);
+    bool same;
     
     // test your code by calling the functions and printing the output 
+    printf("Point 1 is (%.1f,%.1f)\n",p1.x,p1.y);
+    printf("Point 2 is (%.1f,%.1f)\n",p2.x,p2.y);
+    printf("Point 3 is (%.1f,%.1f)\n",p3.x,p3.y);
+    printf("Line length is %.1f\n",lineLength(l1));
+    printf("Triangle area is %.1f\n",triangleArea(t1));
+    same = samePoint(p1,p2);
+    printf("Points 1 and 2 are the same: %s\n", same ? "true" : "false");
     
     return 0;
 }
--- worksheet/portfolio/shapes.c
+++ worksheet/portfolio/shapes.c
@@ -4,9 +4,13 @@
 
 #include "shapes.h"
 
+//Name: Ethan West
+//Student ID: 201990893
+
 Point makePoint( float x, float y ) {
     Point new;
-    // implementation
+    new.x = x;
+    new.y = y;
     return new;
 }
 
@@ -14,3 +18,66 @@ Point makePoint( float x, float y ) {
 // - start with stubs as above
 // - compile regularly to test syntax
 // - test functions by calling them from main()
+
+Line makeLine( Point p1, Point p2 ) {
+    Line new;
+    new.p[0] = p1;
+    new.p[1] = p2;
+    return new;
+}  
+
+Triangle makeTriangle( Point p1, Point p2, Point p3 ) {
+    Triangle new;
+    new.p[0] = p1;
+    new.p[1] = p2;
+    new.p[2] = p3;
+    return new;
+}
+
+float lineLength( Line l ){
+    float length, dx, dy;
+    dx = l.p[1].x - l.p[0].x;
+    dy = l.p[1].y - l.p[0].y;
+    length = sqrt(dx*dx + dy*dy);
+    return length;
+} 
+
+float triangleArea( Triangle t ) {
+    float area, a, b, c, s;
+    a = lineLength(makeLine(t.p[0], t.p[1]));
+    b = lineLength(makeLine(t.p[1], t.p[2]));
+    c = lineLength(makeLine(t.p[2], t.p[0]));
+    s = (a + b + c) / 2;
+    area = sqrt(s * (s - a) * (s - b) * (s - c));
+    return area;
+}
+
+bool samePoint( Point p1, Point p2 ) {
+    bool same;
+    if (abs(lineLength(makeLine(p1,p2)))<1.0e-6) {
+        same = true;
+    } else {
+        same = false;
+    }
+    return same;
+}
+
+bool pointInLine( Point p, Line l) {
+    bool inLine;
+    if (samePoint(p, l.p[0]) || samePoint(p, l.p[1])) {
+        inLine = true;
+    } else {
+        inLine = false;
+    }
+    return inLine;
+}
+
+bool pointInTriangle( Point p, Triangle t ) {
+    bool inTriangle;
+    if (samePoint(p, t.p[0]) || samePoint(p, t.p[1]) || samePoint(p, t.p[2])) {
+        inTriangle = true;
+    } else {
+        inTriangle = false;
+    }
+    return inTriangle;
+}
